d0c49b01c3bb95a72566865ffaab56b1b6282ec0
Alejandro Menéndez <ale240307@gmail.com>
2023-10-31 10:56:00 -0300

    Solucion de repetri moviles y form de salida

--- main.py
+++ main.py
@@ -4,24 +4,20 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user, login_manager
 from datetime import datetime
 
-
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'ale12345678ale'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///guardiaA.db'
 db = SQLAlchemy(app)
 
-
 login_manager = LoginManager()
 login_manager.init_app(app)
 
-
-
-#TABLAS EN BASE DE DATOS:
+# TABLAS EN BASE DE DATOS:
 
 with app.app_context():
     class Agentes(db.Model):
-        id_agente = db.Column(db.Integer, primary_key=True,unique=True)
-        apellido = db.Column(db.String(100) )
+        id_agente = db.Column(db.Integer, primary_key=True, unique=True)
+        apellido = db.Column(db.String(100))
         nombre = db.Column(db.String(100))
         guardia = db.Column(db.String(10))
         dni = db.Column(db.Integer)
@@ -32,12 +28,14 @@ with app.app_context():
         domicilio = db.Column(db.String(300))
         email = db.Column(db.String(100))
         Rol = db.Column(db.String(100))
-        Grupo_sanguineo= db.Column(db.String(100))
+        Grupo_sanguineo = db.Column(db.String(100))
         Alergia = db.Column(db.String(100))
         Camada = db.Column(db.String(10))
         Bombero = db.Column(db.String(1000))
-        registro= db.Column(db.String(1000))
-    #Line below only required once, when creating DB.
+        registro = db.Column(db.String(1000))
+
+
+    # Line below only required once, when creating DB.
     db.create_all()
 
 with app.app_context():
@@ -47,7 +45,9 @@ with app.app_context():
         password = db.Column(db.String(100))
         name = db.Column(db.String(1000))
         telefono = db.Column(db.String(15))
-    #Line below only required once, when creating DB.
+
+
+    # Line below only required once, when creating DB.
     db.create_all()
 
 with app.app_context():
@@ -72,7 +72,6 @@ with app.app_context():
         Buzo = db.Column(db.String(10))
         Campera = db.Column(db.String(10))
 
-
 with app.app_context():
     class Cambiosguardia(db.Model):
         __tablename__ = 'Cambiosguardia'  # Agrega esta línea para especificar el nombre de la tabla en la base de datos
@@ -82,12 +81,12 @@ with app.app_context():
         fecha_devolucion = db.Column(db.Date)
         Motivo = db.Column(db.String(1000))
 
-
 with app.app_context():
     class Movimientos_moviles(db.Model):
         __tablename__ = 'Movimientos_moviles'  # Agrega esta línea para especificar el nombre de la tabla en la base de datos
+        id_movimiento = db.Column(db.Integer, primary_key=True)
         fecha = db.Column(db.Date)
-        numero_movil = db.Column(db.Integer, primary_key=True)
+        numero_movil = db.Column(db.Integer)
         destino = db.Column(db.String(1000))
         hora_salida = db.Column(db.Time)
         km_salida = db.Column(db.Integer)
@@ -96,14 +95,47 @@ with app.app_context():
         a_cargo = db.Column(db.String(50))
         chofer = db.Column(db.String(50))
 
+with app.app_context():
+    class Salida(db.Model):
+        id = db.Column(db.Integer, primary_key=True)
+        nmovil = db.Column(db.String(100))
+        ag_cargo = db.Column(db.String(100))
+        ag_chofer = db.Column(db.String(100))
+        dotacion = db.Column(db.String(100))
+        zona = db.Column(db.String(100))
+        handys = db.Column(db.String(100))
+        gorgi = db.Column(db.Integer)
+        pulasky = db.Column(db.Integer)
+        pala = db.Column(db.Integer)
+        mclood = db.Column(db.Integer)
+        derqui = db.Column(db.Integer)
+        bidon_nafta = db.Column(db.Integer)
+        bolsa_rescate = db.Column(db.String(100))
+        torpedo = db.Column(db.Integer)
+        tabla = db.Column(db.Integer)
+        conos = db.Column(db.Integer)
+        sopladora = db.Column(db.String(100))
+        cajon = db.Column(db.String(100))
+        chaleco = db.Column(db.Integer)
+        odometro = db.Column(db.Integer)
+        bolsos_trauma = db.Column(db.Integer)
+
+
+    db.create_all()
 
 
 
-
-
-
-
-
+@app.route('/salidas', methods=['GET'])
+def salidas():
+    if request.method == 'POST':
+        id_filtro = request.form['id_filtro']
+        if id_filtro:
+            salidas = Salida.query.filter_by(id=id_filtro).all()
+        else:
+            salidas = Salida.query.all()
+    else:
+        salidas = Salida.query.all()
+    return render_template('salidas.html', salidas=salidas)
 
 
 
@@ -113,49 +145,111 @@ with app.app_context():
 def load_user(user_id):
     return User.query.get(int(user_id))
 
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        fecha = datetime.strptime(request.form['fecha'], '%Y-%m-%d').date()  # Convierte la cadena de fecha a objeto de fecha
-        movil = request.form['movil']
-        destino = request.form['destino_actividad']
-        km_salida = request.form['km_salida']
-        km_llegada = request.form['km_llegada']
-        hora_llegada = datetime.strptime(request.form['hora_llegada'], '%H:%M').time()  # Convierte la cadena de hora a objeto de hora
-        hora_salida = datetime.strptime(request.form['hora_salida'], '%H:%M').time()
+        if 'registro_movil' in request.form:
+            fecha = datetime.strptime(request.form['fecha'],
+                                      '%Y-%m-%d').date()  # Convierte la cadena de fecha a objeto de fecha
+            movil = request.form['movil']
+            destino = request.form['destino_actividad']
+            km_salida = request.form['km_salida']
+            km_llegada = request.form['km_llegada']
+            hora_llegada = datetime.strptime(request.form['hora_llegada'],
+                                             '%H:%M').time()  # Convierte la cadena de hora a objeto de hora
+            hora_salida = datetime.strptime(request.form['hora_salida'], '%H:%M').time()
 
-        nuevo_registro = Movimientos_moviles(
-            fecha=fecha,
-            numero_movil=movil,
-            destino=destino,
-            km_salida=km_salida,
-            km_llegada=km_llegada,
-            hora_llegada=hora_llegada,
-            hora_salida=hora_salida,
-            a_cargo=request.form.get('a_cargo').upper(),
-            chofer= request.form.get('chofer').upper()
-        )
+            nuevo_registro = Movimientos_moviles(
+                fecha=fecha,
+                numero_movil=movil,
+                destino=destino,
+                km_salida=km_salida,
+                km_llegada=km_llegada,
+                hora_llegada=hora_llegada,
+                hora_salida=hora_salida,
+                a_cargo=request.form.get('a_cargo').upper(),
+                chofer=request.form.get('chofer').upper()
+            )
 
+            db.session.add(nuevo_registro)
+            db.session.commit()
+            flash('Se registro correctamente el movimiento del movil')
+        if 'parte_salida' in request.form:
+            nmovil = request.form.get('nmovil')
+            ag_cargo = request.form.get('ag_cargo')
+            ag_chofer = request.form.get('ag_chofer')
+            dotacion = request.form.get('dotacion')
+            zona = request.form.get('zona')
 
-        db.session.add(nuevo_registro)
-        db.session.commit()
-        flash('Se registro correctamente el movimiento del movil')
+            handys = request.form.get('handys')  # Obtén el valor del campo "handys" como una cadena
+            handys_list = [h.strip() for h in handys.split(',')]  # Divide los números por comas y crea una lista
+            handys_str = ','.join(handys_list)  # Convierte la lista en una cadena separada por comas
+
+            gorgi = request.form.get('gorgi')
+            pulasky = request.form.get('pulasky')
+            pala = request.form.get('pala')
+            mclood = request.form.get('mclood')
+            derqui = request.form.get('derqui')
+            bidon_nafta = request.form.get('bidon_nafta')
+
+            bolsa_rescate = request.form.get('bolsa_rescate')
+            bolsa_rescate_list = [h.strip() for h in bolsa_rescate.split(',')]
+            bolsa_rescate_str = ','.join(bolsa_rescate_list)
+
+            torpedo = request.form.get('torpedo')
+            tabla = request.form.get('tabla')
+            conos = request.form.get('conos')
+            sopladora = request.form.get('Sopladora')
+            cajon = request.form.get('cajon')
+            chaleco = request.form.get('chaleco')
+            odometro = request.form.get('odometro')
+            bolsos_trauma = request.form.get('bolsos_trauma')
+
+            # Crea una nueva entrada en la base de datos
+            nueva_salida = Salida(
+                nmovil=nmovil,
+                ag_cargo=ag_cargo,
+                ag_chofer=ag_chofer,
+                dotacion=dotacion,
+                zona=zona,
+                handys=handys_str,
+                gorgi=gorgi,
+                pulasky=pulasky,
+                pala=pala,
+                mclood=mclood,
+                derqui=derqui,
+                bidon_nafta=bidon_nafta,
+                bolsa_rescate=bolsa_rescate_str,
+                torpedo=torpedo,
+                tabla=tabla,
+                conos=conos,
+                sopladora=sopladora,
+                cajon=cajon,
+                chaleco=chaleco,
+                odometro=odometro,
+                bolsos_trauma=bolsos_trauma
+            )
+
+            db.session.add(nueva_salida)
+            db.session.commit()
+            flash("Parte de salida registrado correctamente")
 
     return render_template("index.html", moviles=movimientos)
 
 
 @app.route('/movimientos')
 def movimientos():
-    movimientos = Movimientos_moviles.query.order_by(Movimientos_moviles.numero_movil).all()
+    movimientos = Movimientos_moviles.query.order_by(Movimientos_moviles.id_movimiento).all()
 
     return render_template("movimientos.html", moviles=movimientos)
-
 
 
 @app.route('/logout')
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -175,14 +269,10 @@ def login():
         else:
 
             login_user(user)
-            #aca podria ir la tabla de reigistro de cada agente admin
-
+            # aca podria ir la tabla de reigistro de cada agente admin
 
             return redirect(url_for("secrets"))
     return render_template("login.html")
-
-
-
 
 
 @app.route('/register', methods=["GET", "POST"])
@@ -249,13 +339,12 @@ def secrets():
             fecha_ven = request.form.get("fecha_venc")
             try:
                 fechaotor = datetime.strptime(fecha_oto, '%Y-%m-%d').date()
-                fechaoven= datetime.strptime(fecha_ven, '%Y-%m-%d').date()
+                fechaoven = datetime.strptime(fecha_ven, '%Y-%m-%d').date()
             except ValueError:
                 flash(
                     "Error: Formato de fecha incorrecto. Asegúrate de que las fechas estén en el formato 'YYYY-MM-DD'")
                 # Aquí puedes decidir cómo manejar el error, ya sea lanzando una excepción, mostrando un mensaje de error o realizando alguna otra acción apropiada.
                 return redirect(url_for("secrets"))
-
 
             nueva_licencia = CarnetConducir(
                 id_agente=request.form.get("id_agente"),
@@ -267,7 +356,6 @@ def secrets():
             db.session.add(nueva_licencia)
             db.session.commit()
             flash("Se registró correctamente el Carnet")
-
 
         if "registrar_talles" in request.form:
             nuevo_talle = Talles(
@@ -285,8 +373,6 @@ def secrets():
             db.session.add(nuevo_talle)
             db.session.commit()
             flash("Se registraron correctamente los talles solicitados ")
-
-
 
         if "cambiosguardia" in request.form:
             # Convertir las cadenas de fecha en objetos de fecha (date) de Python
@@ -316,35 +402,27 @@ def secrets():
     return render_template("secrets.html", name=current_user, last_three_users=ultimos3_usuarios)
 
 
-
-
-
-
 @app.route('/guardiaA', methods=["GET", "POST"])
 @login_required
 def guardiaA():
     agentesA = Agentes.query.filter(Agentes.guardia == 'A').order_by(Agentes.id_agente).all()
     return render_template("guardiaA.html", lista_agentes=agentesA)
 
+
 @app.route('/licencias_A', methods=["GET", "POST"])
 @login_required
 def licenciasA():
     agentesA = Agentes.query.filter(Agentes.guardia == 'A').order_by(Agentes.id_agente).all()
-    licenciasA= CarnetConducir.query.order_by(CarnetConducir.id_agente).all()
+    licenciasA = CarnetConducir.query.order_by(CarnetConducir.id_agente).all()
     return render_template("licenciasa.html", lista_agentes=agentesA, Licencias=licenciasA)
-
 
 
 @app.route('/talles_A', methods=["GET", "POST"])
 @login_required
 def tallesA():
     agentesA = Agentes.query.filter(Agentes.guardia == 'A').order_by(Agentes.id_agente).all()
-    provistosA= Talles.query.order_by(Talles.id_agente).all()
+    provistosA = Talles.query.order_by(Talles.id_agente).all()
     return render_template("provistosa.html", lista_agentes=agentesA, provistos=provistosA)
-
-
-
-
 
 
 @app.route('/guardiaB', methods=["GET", "POST"])
@@ -353,21 +431,21 @@ def guardiaB():
     agentesB = Agentes.query.filter(Agentes.guardia == 'B').order_by(Agentes.id_agente).all()
     return render_template("guardiaB.html", lista_agentes=agentesB)
 
+
 @app.route('/licencias_B', methods=["GET", "POST"])
 @login_required
 def licenciasB():
     agentesB = Agentes.query.filter(Agentes.guardia == 'B').order_by(Agentes.id_agente).all()
-    licenciasB= CarnetConducir.query.order_by(CarnetConducir.id_agente).all()
+    licenciasB = CarnetConducir.query.order_by(CarnetConducir.id_agente).all()
     return render_template("licenciasb.html", lista_agentes=agentesB, Licencias=licenciasB)
+
 
 @app.route('/talles_B', methods=["GET", "POST"])
 @login_required
 def tallesB():
     agentesB = Agentes.query.filter(Agentes.guardia == 'B').order_by(Agentes.id_agente).all()
-    provistosB= Talles.query.order_by(Talles.id_agente).all()
+    provistosB = Talles.query.order_by(Talles.id_agente).all()
     return render_template("provistosb.html", lista_agentes=agentesB, provistos=provistosB)
-
-
 
 
 @app.route('/guardiaC', methods=["GET", "POST"])
@@ -376,24 +454,27 @@ def guardiaC():
     agentesC = Agentes.query.filter(Agentes.guardia == 'C').order_by(Agentes.id_agente).all()
     return render_template("guardiaC.html", lista_agentes=agentesC)
 
+
 @app.route('/licencias_C', methods=["GET", "POST"])
 @login_required
 def licenciasC():
     agentesC = Agentes.query.filter(Agentes.guardia == 'C').order_by(Agentes.id_agente).all()
-    licenciasC= CarnetConducir.query.order_by(CarnetConducir.id_agente).all()
+    licenciasC = CarnetConducir.query.order_by(CarnetConducir.id_agente).all()
     return render_template("licenciasc.html", lista_agentes=agentesC, Licencias=licenciasC)
+
 
 @app.route('/talles_C', methods=["GET", "POST"])
 @login_required
 def tallesC():
     agentesC = Agentes.query.filter(Agentes.guardia == 'C').order_by(Agentes.id_agente).all()
-    provistosC= Talles.query.order_by(Talles.id_agente).all()
+    provistosC = Talles.query.order_by(Talles.id_agente).all()
     return render_template("provistosc.html", lista_agentes=agentesC, provistos=provistosC)
+
 
 @app.route('/editar_licencia/<int:id_agente>', methods=['GET', 'POST'])
 @login_required
 def editar_licencia(id_agente):
-    licencia = CarnetConducir.query.all()
+    licencia = CarnetConducir.query.filter_by(id_agente=id_agente).first()
 
     if request.method == 'POST':
         fecha_otorgamiento = request.form.get('fecha_otorgamiento')
@@ -414,22 +495,9 @@ def editar_licencia(id_agente):
     return render_template('editar_licencia.html', licencia=licencia)
 
 
-
-
-
-
-
-
-
 @app.route('/moviles')
 def moviles():
     return render_template("moviles.html")
-
-
-
-
-
-
 
 
 if __name__ == '__main__':
